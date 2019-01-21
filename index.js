dcc65beaea461bad430137b514aa1ef4a5bd407c
Indre <indre.vailionyte@moovel.com>
2019-01-21 11:01:40 +0100

    Feat: Create new user

--- index.js
+++ index.js
@@ -4,29 +4,37 @@ const app = express();
 
 const router = express.Router();
 
+const users = [
+    {
+        "id": "2976a4e8-9f2f-4a45-9f9f-5a56dd47b1ca",
+        "name": "Jenny Doe"
+    },
+    {
+        "id": "6a034cbd-88b8-4e9e-b94d-cfdab7c06b9b",
+        "name": "John Doe"
+    }
+];
+
 router.route('/api/v1/users')
-.get((request, response) => {
-    const users = {
-        "users": [
-          {
-            "id": "2976a4e8-9f2f-4a45-9f9f-5a56dd47b1ca",
-            "name": "Jenny Doe"
-          },
-          {
-            "id": "6a034cbd-88b8-4e9e-b94d-cfdab7c06b9b",
-            "name": "John Doe"
-          }
-        ]
-      };
+    .get((request, response) => {
+        response.json({
+            users: users,
+        })
+    })
 
-    response.json({
-        users: users,
+    .post((request, response) => {
+        const user = {
+           "id": "2976a4e8-9f2f-4a45-9f9f-5a56dd47b1bb",
+            "name": "Jane Doe"
+        };
+        
+        users.push(user);
+
+        response.json({
+            user: user,
+        })
     });
-})
-
 
 app.use(router);
 
-
 app.listen(3000);
-
