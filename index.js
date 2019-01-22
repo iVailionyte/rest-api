362b51cc687f573c43dfa273949d8a6b3839e6ab
Indre <indre.vailionyte@moovel.com>
2019-01-22 11:20:17 +0100

    Feat: Get user by ID

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const router = express.Router();
 const users = [
     {
         "id": "2976a4e8-9f2f-4a45-9f9f-5a56dd47b1ca",
-        "name": "Jenny Doe"
+        "name": "jenny-doe"
     },
     {
         "id": "6a034cbd-88b8-4e9e-b94d-cfdab7c06b9b",
-        "name": "John Doe"
+        "name": "john-doe"
     }
 ];
 
@@ -25,7 +25,7 @@ router.route('/api/v1/users')
     .post((request, response) => {
         const user = {
            "id": "2976a4e8-9f2f-4a45-9f9f-5a56dd47b1bb",
-            "name": "Jane Doe"
+            "name": "jane-doe"
         };
         
         users.push(user);
@@ -34,6 +34,17 @@ router.route('/api/v1/users')
             user: user,
         })
     });
+
+router.route('/api/v1/users/:id')
+    .get((request, response) => {
+        const singleUser = users.find(function(single) {
+            return single.name === request.params.id;
+        });
+
+        response.json({
+            user: singleUser,
+        })
+    })
 
 app.use(router);
 
